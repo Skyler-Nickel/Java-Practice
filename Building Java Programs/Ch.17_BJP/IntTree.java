6011bc8ed24bd5c975fda0a85574e623c095903b
Skyler-Nickel <138588294+Skyler-Nickel@users.noreply.github.com>
2024-04-17 15:40:28 -0700

    About halfway done with the chapter 17 exercises

--- Building Java Programs/Ch.17_BJP/IntTree.java
+++ Building Java Programs/Ch.17_BJP/IntTree.java
@@ -131,4 +131,150 @@ public class IntTree {
             return countLeaves(root.left) + countLeaves(root.right);
         }
     }
+
+    // Exercise 1:
+    // Write a method called countLeftNodes that returns the number of left children in the tree.
+    // A left child is a node that appears as the root of the left hand subtree of another node.
+    public int countLeftNodes() {
+        return countLeftNodes(overallRoot);
+    }
+
+    private int countLeftNodes(IntTreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null) {
+            return 1;
+        } else if (root.left != null) {
+            return 1 + countLeftNodes(root.left) + countLeftNodes(root.right);
+        } else {
+            return countLeftNodes(root.left) + countLeftNodes(root.right);
+        }
+    }
+
+    // Exercise 2:
+    // Write a method called countEmpty that returns the number of empty branches in a tree. An
+    // empty tree is considered to have empty branch (the tree itself). For non empty trees, your
+    // method should count the total number of empty branches amoung the nodes of the tree.
+    public int countEmpty() {
+        return countEmpty(overallRoot);
+    }
+
+    private int countEmpty(IntTreeNode root) {
+        if (root == null) {
+            return 1;
+        } else {
+            return countEmpty(root.left) + countEmpty(root.right);
+        }
+    }
+
+    // Exercise 3:
+    // Write a method called depthSum that returns the sum of the values stored in a binary
+    // tree of integers weighted by the depth of each value. The method should return the value
+    // at the root, plus 2 times the values stored at next level of tree, plus 3 times the values
+    // stored at the next level of the tree, and so on.
+    public int depthSum() {
+        return depthSum(overallRoot, 1);
+    }
+
+    private int depthSum(IntTreeNode root, int level) {
+        if (root == null) {
+            return 0;
+        } else {
+            return level * root.data + (depthSum(root.left, level + 1) + depthSum(root.right, level + 1));
+        }
+    }
+
+    // Exercise 4:
+    // Write a method called countEvenBranches that returns the number of branch nodes in a
+    // binary tree that contains even numbers. A branch node has one or two children.
+    public int countEvenBranches() {
+        return countEvenBranches(overallRoot);
+    }
+
+    private int countEvenBranches(IntTreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.data % 2 == 0  && (root.left != null || root.right != null)) {
+            return 1 + countEvenBranches(root.left) + countEvenBranches(root.right);
+        } else {
+            return countEvenBranches(root.left) + countEvenBranches(root.right);
+        }
+    }
+
+    // Exercise 5:
+    // Write a method called printLevel that accepts an integer parameter n and prints the values
+    // at level n from left to right, one per line. We will use the convention that the overall
+    // root is at level 1, its children are at level 2, and so on.
+    public void printLevel(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }
+        printLevel(overallRoot, n);
+    }
+
+    private void printLevel(IntTreeNode root, int n) {
+        if (root == null) {
+            return;
+        } else if (n == 1) {
+            System.out.println(root.data);
+            return;
+        } else {
+            printLevel(root.left, n - 1);
+            printLevel(root.right, n - 1);
+        }
+    }
+
+    // Exercise 6:
+    // Write a method called printLeaves that prints to System.out the leaves of a binary tree
+    // from left to right. More specifically, the leaves should be printed in the reverse order
+    // that would be printed using any of the standard traversals. If the tree is empty,
+    // your method should produce the output "no leaves".
+    public void printLeaves() {
+        if (overallRoot == null) {
+            System.out.println("no leaves");
+            return;
+        } else {
+            System.out.print("leaves:");
+            printLeaves(overallRoot);
+    
+        }
+    }
+
+    private void printLeaves(IntTreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            System.out.print(" " + root.data);
+            return;
+        }
+        printLeaves(root.right);
+        printLeaves(root.left);
+    }
+
+    // Exercise 7:
+    // Write a method called isFull that returns true if a binary tree is full and false
+    // if it is not. A full binary tree is one in which every node has 0 or 2 children.
+    public boolean isFull() {
+        return isFull(overallRoot);
+    }
+
+    private boolean isFull(IntTreeNode root) {
+        if (root == null) {
+            return true;
+        } else if (root.left != null && root.right == null) {
+            return false;
+        } else if (root.left == null && root.right != null) {
+            return false;
+        }
+        return isFull(root.left) && isFull(root.right);
+    }
+
+    // Exercise 8:
+    // Write a toString method for a binary tree of integers. The method should return "empty"
+    // for an empty tree. For a leaf node, it should return the data in the node as a string/
+    // For a branch node, it should return a parenthesized String that has three elements
+    // separated by commas: the data at the root, a string representation of the left subtree,
+    // and then a string representation of the right subtree.
+    
 }
